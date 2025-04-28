9ffcaf6e52738881770c9524d2b059a1d91471e4
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-04-28 22:25:47 +0300

    Structurize editor, create Title.jsx output

---
+++ src/Editor/Editor.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import Title from "./Title";
+
+function Editor() {
+  return (
+    <div className="editor">
+      <Title />
+    </div>
+  );
+}
+
+export default Editor;
---
+++ src/Editor/Title.jsx
@@ -0,0 +1,12 @@
+function Title() {
+  return (
+    <>
+      <label htmlFor="fullName">Full Name:</label>
+      <input type="text" id="fullName" name="fullName" />
+      <label htmlFor="position">Position:</label>
+      <input type="text" id="position" name="position" />
+    </>
+  );
+}
+
+export default Title;
