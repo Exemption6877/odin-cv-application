92fadbeb8a742de38ae902a98d7e0623e4e64ed3
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-04 07:11:13 +0300

    Add summary logic

--- src/Layout/components/Summary.jsx
+++ src/Layout/components/Summary.jsx
@@ -1,12 +1,52 @@
+import "../../styles/Summary.css";
+import IconButton from "../../components/IconButton";
+import { useState } from "react";
+
 function Summary() {
+  const [entry, setEntry] = useState("");
+  const [isEditing, setIsEditing] = useState(true);
+
+  function handleTyping(e) {
+    setEntry(e.target.value);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    setEntry(entry.trim());
+    setIsEditing(!isEditing);
+  }
+
   return (
     <div className="summary container">
       <h2>Profile Summary</h2>
-      <form>
-        <label htmlFor="summary">Profile Summary</label>
-        <textarea name="summary" id="summary"></textarea>
-        <input type="submit" />
-      </form>
+      {isEditing ? (
+        <form>
+          <label htmlFor="summary">Profile Summary</label>
+          <textarea
+            name="summary"
+            id="summary"
+            onChange={handleTyping}
+            value={entry}
+          ></textarea>
+          <IconButton
+            type="submit"
+            name="skills-submit"
+            text="Submit"
+            onClick={handleClick}
+          />
+        </form>
+      ) : (
+        <>
+          <p>{entry}</p>
+          <IconButton
+            type="edit"
+            name="summary-edit"
+            id={`summary-edit`}
+            valu="edit"
+            onClick={handleClick}
+          />
+        </>
+      )}
     </div>
   );
 }
