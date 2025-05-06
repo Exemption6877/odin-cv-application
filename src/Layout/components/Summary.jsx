ae3a7e8eba435a26a2b68aa5bea46145bc2c929c
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-06 21:01:38 +0300

    Refactor Summary.jsx/css

--- src/Layout/components/Summary.jsx
+++ src/Layout/components/Summary.jsx
@@ -1,24 +1,50 @@
 import "../../styles/Summary.css";
 import IconButton from "../../components/IconButton";
 import { useState } from "react";
+import Alert from "../../components/alert";
 
 function Summary() {
-  const [entry, setEntry] = useState("");
+  const [entry, setEntry] = useState({
+    summary: "",
+  });
+
   const [isEditing, setIsEditing] = useState(true);
+  const [errors, setErrors] = useState([]);
 
   function handleTyping(e) {
-    setEntry(e.target.value);
+    const { id, value } = e.target;
+    setErrors([]);
+
+    setEntry((prev) => ({ ...prev, [id]: value }));
   }
 
   function handleClick(e) {
     e.preventDefault();
-    setEntry(entry.trim());
+
+    const currentErrors = [];
+
+    if (entry.summary.trim().length === 0) {
+      currentErrors.push(`Please enter your summary.`);
+    }
+
+    if (currentErrors.length > 0) {
+      setErrors(currentErrors);
+      return;
+    }
+
+    setErrors([]);
+    setEntry({
+      summary: entry.summary.trim(),
+    });
+
     setIsEditing(!isEditing);
   }
 
   return (
     <div className="summary container">
       <h2>Profile Summary</h2>
+      {errors.length > 0 ? <Alert message={errors[0]} /> : null}
+
       {isEditing ? (
         <form>
           <label htmlFor="summary">Profile Summary</label>
@@ -26,18 +52,19 @@ function Summary() {
             name="summary"
             id="summary"
             onChange={handleTyping}
-            value={entry}
+            value={entry.summary}
+            placeholder="Your summary..."
           ></textarea>
           <IconButton
             type="submit"
-            name="skills-submit"
+            name="summary-submit"
             text="Submit"
             onClick={handleClick}
           />
         </form>
       ) : (
         <>
-          <p>{entry}</p>
+          <p>{entry.summary}</p>
           <IconButton
             type="edit"
             name="summary-edit"
